119507d8209c66d73f35c332a1b41a89b1149c66
Federico Krum <federico@krum.com.ar>
2015-08-29 17:46:30 -0300

    Update vestacp.sh

--- vestacp.sh
+++ vestacp.sh
@@ -2,20 +2,44 @@
 #
 # Minimal requirements
 # --------------------
-# 
 # A 5 dollars a month Digital Ocean Ubuntu 14.04 64bit server ()
-512 mb of ram
-
+# 512 mb ram / 1 CPU
+# 20 GB SSD Disk
+# 1000 GB Transfer
+# Get it at with a 10 dollar rebate at https://www.digitalocean.com/?refcode=4c039b01c48f
+#
+# After login to your droplet for the first time, copy the line bellow (withouth the first #) and execute it in the terminal
 # curl -O https://github.com/FastDigitalOceanDroplets/VestaCP/blob/master/vestacp.sh && bash vestacp.sh
+#
+# When this other process finishes, do the same with the next line in the server
+# curl -O https://github.com/FastDigitalOceanDroplets/VestaCP/blob/master/vestacp.sh && bash vestacp.sh
+
+
+# Prevents doing this from other account than root
 if [ "x$(id -u)" != 'x0' ]; then
     echo 'Error: this script can only be executed by root'
     exit 1
 fi
+##################################
+# Interactive Part
+# Get admin's email
+email=""
+$valid_email=[ "$email" =~ "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$" ]
+while [$valid_email]
+do
+    read -p "Enter admin email: " email
+	echo
+    if [$valid_email]
+    then 
+        echo "Email address $email is valid."
+    else
+        echo "Email address $email is invalid." 
+    fi
+done
 
+exit
 
-
-
-# -------------- Creats SWAP
+# Creates SWAP on the server
 sudo fallocate -l 512M /swapfile
 sudo chmod 600 /swapfile
 sudo mkswap /swapfile
@@ -26,11 +50,10 @@ sudo echo "vm.swappiness=10" >> /etc/sysctl.conf
 sudo sysctl vm.vfs_cache_pressure=50
 sudo echo "vm.vfs_cache_pressure=50" >> /etc/sysctl.conf
 
-# sudo apt-get install ntp # Solo en servidores reales, no en Digital Ocean
-sudo timedatectl set-timezone America/Argentina/Buenos_Aires
+# Change time zone at your new server
+dpkg-reconfigure tzdata
 
-#curl -O https://dl.dropboxusercontent.com/u/xxx/Digital_Ocean_Vesta/vesta_posti.sh
-
+# Set the locale on your computer (is not the smartest way, I accept sugestions to do it interactivily)
 export LC_ALL=en_US.UTF-8
 export LANG="en_US.UTF-8"
 export LANGUAGE=en_US.UTF-8
@@ -38,26 +61,27 @@ echo 'LC_ALL=en_US.UTF-8' >> /etc/environment
 echo 'LANG=en_US.UTF-8' >> /etc/environment
 echo 'LANGUAGE=en_US.UTF-8' >> /etc/environment
 
+# Update all your server software
 apt-get update
 apt-get upgrade
-apt-get dist-upgrade
-apt-get autoremove
+# apt-get dist-upgrade
+# apt-get autoremove
 
+# Reconfigure locale
 apt-get install --reinstall language-pack-en
 locale-gen
 locale-gen en_US.UTF-8
 dpkg-reconfigure locales
 
+# Change root password for your server
 clear
 echo "Cambiamos la clave de root"
 passwd root
 echo
 
-# email="youremail@gmail.com"
-# echo "Instalamos VestaCP con $email como email de admin"
+# install vesta with admin's email
 curl -O http://vestacp.com/pub/vst-install.sh
-bash vst-install.sh 
-# -e $email
+bash vst-install.sh -e $email
 
 # De aca para abajo no se ejecuta nada
 # Arreglar el MySQL
