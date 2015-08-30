b297bd39a58ce05e71b00a8593610a71fe29c2d1
Federico Krum <federico@krum.com.ar>
2015-08-30 02:28:29 -0300

    Create vestacp_post.sh

---
+++ vestacp_post.sh
@@ -0,0 +1,4 @@
+
+# The line below helped me to prevent mysql server from stoping by itselfe often.
+# execute it in your server
+service mysql stop && service mysql start && dpkg-reconfigure mysql-server-5.5
