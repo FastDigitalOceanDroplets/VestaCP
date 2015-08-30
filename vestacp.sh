d71dbb3089619396dc4ae8b7ccb29918e0c900df
Federico Krum <federico@krum.com.ar>
2015-08-30 09:30:35 -0300

    Commit .sublime-text-git-autocommit

--- vestacp.sh
+++ vestacp.sh
@@ -102,9 +102,11 @@ do
         if [[ "$email" =~ ^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$ ]]
         then
             echo "Email address $email is valid."
+            echo
             break
         else
             echo "Email address $email is invalid."
+            echo
         fi
     done
 
