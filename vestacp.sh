b42fcf7be33ab1c56d2b7dec421d1241ea384a90
Federico Krum <federico@krum.com.ar>
2015-08-30 10:01:13 -0300

    Auto-committing 'vestacp.sh'

--- vestacp.sh
+++ vestacp.sh
@@ -95,7 +95,6 @@ do
     while true
     do
         read -p "Enter Vesta admin email: " email
-        echo
         if [[ "$email" =~ ^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$ ]]
         then
             echo "Email address $email is valid."
