f1a2dc329fb7eab1839a457b8713629a1f763c35
Federico Krum <federico@krum.com.ar>
2016-12-05 11:54:49 +0100

    Update vestacp.sh

--- vestacp.sh
+++ vestacp.sh
@@ -121,19 +121,6 @@ do
     fi
 done
 
-# Creates SWAP on the server
-# One of the things that I have lerned is that this kind of servers need swap.
-# These fast SSD disks do this even more dicirable to have.
-sudo fallocate -l 512M /swapfile
-sudo chmod 600 /swapfile
-sudo mkswap /swapfile
-sudo swapon /swapfile
-sudo echo "/swapfile   none    swap    sw    0   0" >> /etc/fstab
-sudo sysctl vm.swappiness=10
-sudo echo "vm.swappiness=10" >> /etc/sysctl.conf
-sudo sysctl vm.vfs_cache_pressure=50
-sudo echo "vm.vfs_cache_pressure=50" >> /etc/sysctl.conf
-
 # Change time zone at your new server
 dpkg-reconfigure tzdata
 
