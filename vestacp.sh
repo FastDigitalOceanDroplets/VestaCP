f42abd3f113c2bd97ef13e178071b2aa99287507
Federico Krum <federico@krum.com.ar>
2016-12-05 12:18:59 +0100

    Update vestacp.sh

--- vestacp.sh
+++ vestacp.sh
@@ -121,6 +121,19 @@ do
     fi
 done
 
+# Creates SWAP on the server
+# One of the things that I have lerned is that this kind of servers need swap.
+# These fast SSD disks do this even more dicirable to have.
+sudo fallocate -l 512M /swapfile
+sudo chmod 600 /swapfile
+sudo mkswap /swapfile
+sudo swapon /swapfile
+sudo echo "/swapfile   none    swap    sw    0   0" >> /etc/fstab
+sudo sysctl vm.swappiness=10
+sudo echo "vm.swappiness=10" >> /etc/sysctl.conf
+sudo sysctl vm.vfs_cache_pressure=50
+sudo echo "vm.vfs_cache_pressure=50" >> /etc/sysctl.conf
+
 # Change time zone at your new server
 dpkg-reconfigure tzdata
 
@@ -151,3 +164,5 @@ nano /etc/apt/apt.conf.d/50unattended-upgrades
 # install vesta with admin's email
 curl -O http://vestacp.com/pub/vst-install.sh
 bash vst-install.sh -e $email
+Contact GitHub API Training Shop Blog About
+© 2016 GitHub, Inc. Terms Privacy Security Status Help
