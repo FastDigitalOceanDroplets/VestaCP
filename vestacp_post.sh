24565e49bc2fc040cf0aa3c429c0f286eb5161c4
Federico Krum <federico@krum.com.ar>
2016-12-18 10:44:56 +0100

    Delete vestacp_post.sh

--- vestacp_post.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# The line below helped me to prevent mysql server from stoping by itselfe often.
-# execute it in your server
-service mysql stop && service mysql start && dpkg-reconfigure mysql-server-5.5
