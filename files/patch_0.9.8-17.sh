2e46e48592e462ddf4023de64fde03e4d2f7da77
Federico Krum <federico@krum.com.ar>
2016-12-18 13:59:35 +0100

    Update patch_0.9.8-17.sh

--- files/patch_0.9.8-17.sh
+++ files/patch_0.9.8-17.sh
@@ -3,3 +3,4 @@
 #patch to fix mysql md5 passwords missing on restore
 curl https://raw.githubusercontent.com/serghey-rodin/vesta/04d617d756656829fa6c6a0920ca2aeea84f8461/func/db.sh > /usr/local/vesta/func/db.sh
 curl https://raw.githubusercontent.com/serghey-rodin/vesta/04d617d756656829fa6c6a0920ca2aeea84f8461/func/rebuild.sh > /usr/local/vesta/func/rebuild.sh
+reboot
