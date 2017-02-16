3f9a219d26d7849fdb95f75d47fc4eae432f9758
Federico Krum <federico@krum.com.ar>
2017-02-16 22:40:20 +0100

    Update post.sh
    Added notice on what system is installed an what patches are being applyed

--- files/post.sh
+++ files/post.sh
@@ -30,12 +30,11 @@ done
 
 # Get Vestas Installed version
 installedversion=`apt-cache policy vesta | grep "Installed"`
-
+echo "$installedversion"
 if [ "$installedversion" == "  Installed: 0.9.8-17" ]; then
-
-#patch to fix mysql md5 passwords missing on restore
-curl https://raw.githubusercontent.com/serghey-rodin/vesta/04d617d756656829fa6c6a0920ca2aeea84f8461/func/db.sh > /usr/local/vesta/func/db.sh
-curl https://raw.githubusercontent.com/serghey-rodin/vesta/04d617d756656829fa6c6a0920ca2aeea84f8461/func/rebuild.sh > /usr/local/vesta/func/rebuild.sh
-reboot
-
+	echo "Applying: 0.9.8-17"
+	#patch to fix mysql md5 passwords missing on restore
+	curl https://raw.githubusercontent.com/serghey-rodin/vesta/04d617d756656829fa6c6a0920ca2aeea84f8461/func/db.sh > /usr/local/vesta/func/db.sh
+	curl https://raw.githubusercontent.com/serghey-rodin/vesta/04d617d756656829fa6c6a0920ca2aeea84f8461/func/rebuild.sh > /usr/local/vesta/func/rebuild.sh
+	reboot
 fi
